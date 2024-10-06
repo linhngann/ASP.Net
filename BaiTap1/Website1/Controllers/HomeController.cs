87fe95a93a047a84a07c6da4836653914d739318
linh ngan <linhhngann07@gmail.com>
2024-10-07 01:27:02 +0700

    add them cac package

---
+++ BaiTap1/Website1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 1 Controller có nhiều action
+        // mỗi 1 action chỉ có 1 view
+        // empty: web rỗng, chưa chạy được
+        // mvc: web có sẵn, chạy được
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HienThiTen()
+        {
+            return View();
+        }
+    }
+}
