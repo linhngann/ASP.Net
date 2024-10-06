e639b55e68a847b33960168ee71fae595f9fae2e
linh ngan <linhhngann07@gmail.com>
2024-10-07 03:12:31 +0700

    Bai 3: dieu huong cac trang

---
+++ BaiTap1/BaiTap2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // chuyển hướng qua action khác, cùng controller
+        // return RedirectToAction("tên action")
+        // chuyển hướng qua action khác, khác controller
+        // return RedirectToAction("tên action", "tên controller")
+        // chuyển hướng qua route khác
+        // return RedirectToRoute("Tên route")
+        // chuyển hướng qua url
+        // return Redirect("tên url")
+        public ActionResult TrangChu()
+        {
+            /*return RedirectToAction("Index", "TinTuc");*/
+            return View();
+            /* return RedirectToRoute("TinTuc");*/
+            /*return Redirect("https://www.google.com/");*/
+        }
+        public ActionResult ThongTin()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BaiTap1/Website1/Controllers/TinTucController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website1.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: TinTuc
+        public ActionResult BanTin()
+        {
+            return View();
+        }
+    }
+}
