5656e3c81a9766ca3ba337e19d46bb5f74cc4761
linh ngan <linhhngann07@gmail.com>
2024-10-14 01:38:01 +0700

    Truyen du lieu tu Controller sang View

--- Baitap2/Baitap2/Controllers/HomeController.cs
+++ Baitap2/Baitap2/Controllers/HomeController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Baitap2.Helper;
 namespace Baitap2.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            // Cách truyền dữ liệu từ Controller sang View
+            //Cách 1: Truyền bằng ViewBag
+            int a = 10;
+            ViewBag.sum = new MathHelper().tinhTong(3, 5);
+            //Cách 2: Truyền qua tham số của hàm View
+            var sv1 = new SinhVien();
+            sv1.ID = "001";
+            sv1.Name = "Linh Ngân";
+            sv1.Description = "Xinh";
+
+            var sv2 = new SinhVien();
+            sv1.ID = "002";
+            sv1.Name = "Minh Nghĩa";
+            sv1.Description = "xấu";
+
+            List<SinhVien> sdList = new List<SinhVien>();
+
+            sdList.Add(sv1);
+            sdList.Add(sv2);
+            return View(sdList);
+
         }
+        
     }
 }
---
+++ Baitap2/Baitap2/Helper/MathHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Baitap2.Helper
+{
+    public class MathHelper
+    {
+        public int tinhTong(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
