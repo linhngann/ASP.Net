f06c80ee5a417c0b0f732d0930ec282c1f496a32
linh ngan <linhhngann07@gmail.com>
2024-10-14 03:10:18 +0700

    thuc hanh truyen du lieu C->V

---
+++ BaiTap3/BaiTap3/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiTap3.Models;
+
+namespace BaiTap3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            MayTinh mt = new MayTinh();
+            List<MayTinh> ds = mt.khoitao();
+            return View(ds);
+        }
+        public ActionResult DanhSach2()
+        {
+            MayTinh mt = new MayTinh();
+            List<MayTinh> ds = mt.khoitao().OrderBy(m=>m.GiaBan).Take(2).ToList();
+            return View(ds);
+        }
+        public ActionResult SapXep()
+        {
+            MayTinh mt = new MayTinh();
+            List<MayTinh> ds = mt.khoitao().OrderBy(m => m.GiaBan).ToList();
+            return View(ds);
+        }
+        public ActionResult Ten()
+        {
+            MayTinh mt = new MayTinh();
+            List<MayTinh> ds = mt.khoitao().Where(m=>m.TenMay=="asus").ToList();
+            return View(ds);
+        }
+    }
+}
---
+++ BaiTap3/BaiTap3/Models/MayTinh.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap3.Models
+{
+    public class MayTinh
+    {
+        public string MaMay {  get; set; }
+        public string TenMay {  get; set; }
+        public double GiaBan {  get; set; }
+        public DateTime NgaySanXuat {  get; set; }
+        public string HangSanXuat {  get; set; }
+
+        public List<MayTinh> khoitao()
+        {
+            List<MayTinh> ds = new List<MayTinh>();
+            ds.Add(new MayTinh
+            {
+                MaMay = "001",
+                TenMay = "HP fff",
+                GiaBan = 120000,
+                NgaySanXuat = new DateTime(2012, 3, 15),
+                HangSanXuat = "Lenovo",
+            });
+            ds.Add(new MayTinh
+            {
+                MaMay = "002",
+                TenMay = "HP ccc",
+                GiaBan = 340000,
+                NgaySanXuat = new DateTime(2012, 3, 15),
+                HangSanXuat = "Lenovo",
+            });
+            ds.Add(new MayTinh
+            {
+                MaMay = "003",
+                TenMay = "asus",
+                GiaBan = 120000,
+                NgaySanXuat = new DateTime(2012, 3, 15),
+                HangSanXuat = "Asus",
+            });
+            ds.Add(new MayTinh
+            {
+                MaMay = "004",
+                TenMay = "HP fff",
+                GiaBan = 170000,
+                NgaySanXuat = new DateTime(2012, 3, 15),
+                HangSanXuat = "Lenovo",
+            });
+            ds.Add(new MayTinh
+            {
+                MaMay = "005",
+                TenMay = "HP fff",
+                GiaBan = 670000,
+                NgaySanXuat = new DateTime(2012, 3, 15),
+                HangSanXuat = "Lenovo",
+            });
+            return ds;
+
+        }
+    }
+}
